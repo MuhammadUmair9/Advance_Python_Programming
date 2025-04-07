36e16e42b1335fcc26e5cb73da8db056fb34dd10
MuhammadUmair9 <umairphysicst@gmail.com>
2025-04-07 18:11:08 +0500

    Add files via upload

---
+++ class exercise.py
@@ -0,0 +1,9 @@
+#Question1
+my_list = [1, 3, 4, 5, 6, 7, 8, 9, 11, 15, 17, 25, 33, 57]
+minimum = min(my_list)
+maximum = max(my_list)
+Average = sum(my_list) /len(my_list)
+
+print("Minimum number is:", minimum)
+print("Maximum number is:", maximum)
+print("Average is:", Average)
