bb683555c04af582133dc7c350c4ab2e39f113ec
MuhammadUmair9 <umairphysicst@gmail.com>
2025-04-07 18:14:43 +0500

    Update class exercise.py

--- class exercise.py
+++ class exercise.py
@@ -7,3 +7,8 @@ Average = sum(my_list) /len(my_list)
 print("Minimum number is:", minimum)
 print("Maximum number is:", maximum)
 print("Average is:", Average)
+
+#Question2
+my_list = [1, 3, 4, 5, 6, 7, 8, 9, 11, 15, 17, 25, 33, 57]
+my_list.sort()
+print(my_list)
